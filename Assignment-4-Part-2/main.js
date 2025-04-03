de7ff0f40cc95777c8c4ca71ee4f7230285c1e40
Nolan Makinen <makinennolan03@gmail.com>
2025-04-03 14:22:25 -0400

    Loops through the images

--- Assignment-4-Part-2/main.js
+++ Assignment-4-Part-2/main.js
@@ -6,13 +6,19 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
 /* Declaring the alternative text for each image file */
 
-/* Looping through images */
+const altText = ["A human eye", "A rock", "Flowers", "Ancient Egypt Wall Mural", "A butterfly"];
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+/* Looping through images */
+images.forEach((image, index) => {
+	const newImage = document.createElement('img');
+	const imgPath = `images/${image}`;
+	newImage.setAttribute('src', imgPath);
+	newImage.setAttribute('alt', altText[index]);
+	thumbBar.appendChild(newImage);
+})
 
 /* Wiring up the Darken/Lighten button */

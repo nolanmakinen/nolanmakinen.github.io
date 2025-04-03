8d74a76e5f41005fa55ae5cc90c9085aa7e77914
Nolan Makinen <makinennolan03@gmail.com>
2025-04-03 15:08:16 -0400

    Added the Darker / Lighten Button

--- Assignment-4-Part-2/main.js
+++ Assignment-4-Part-2/main.js
@@ -26,3 +26,16 @@ images.forEach((image, index) => {
 })
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
